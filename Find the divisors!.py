58be45e9c9cbb23992dc03b358f321b5cf884fc8
C.RONALDO <mutikn@mai.ru>
2023-12-10 01:23:13 +0400

    codewars-Odd or even.iml

---
+++ Find the divisors!.py
@@ -0,0 +1,10 @@
+def divisors(integer):
+    divisor = 2
+    list_of_divisors = []
+    for i in range(2, integer):
+        if integer % divisor == 0:
+            list_of_divisors.append(divisor)
+        divisor += 1
+    return list_of_divisors if len(list_of_divisors) > 0 else f'{integer} is prime'
+
+print(divisors(13))
